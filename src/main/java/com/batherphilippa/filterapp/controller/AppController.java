e61e2cd88558c34a616ec7b0ad74f3446bf7dc8d
pabather <p.a.bather1@gmail.com>
2023-11-27 14:58:42 +0100

    refactorizado nombres de groupid y main class

---
+++ src/main/java/com/batherphilippa/filterapp/controller/AppController.java
@@ -0,0 +1,44 @@
+package com.philippabather.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.ToggleGroup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppController implements Initializable {
+
+    @FXML
+    private RadioButton rbtnOneFile;
+
+    @FXML
+    private ToggleGroup fileSelection;
+
+    @FXML
+    private RadioButton rbtnMultipleFiles;
+
+    @FXML
+    private TabPane tpFilterTabManager;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("App controller initialised...");
+    }
+
+    @FXML
+    void selectFile(ActionEvent event) {
+
+        if (rbtnOneFile.isSelected()) {
+            System.out.println("One file is to be chosen.");
+        } else {
+            System.out.println("Multiples are to be chosen.");
+        }
+
+    }
+
+
+}
