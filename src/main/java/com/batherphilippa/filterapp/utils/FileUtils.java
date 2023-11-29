f0b655deab34793d4f972bc93695ab6d4bf2589f
pabather <p.a.bather1@gmail.com>
2023-11-29 23:22:00 +0100

    añadé métodos estaticos para manejar archivos

--- src/main/java/com/batherphilippa/filterapp/utils/FileUtils.java
+++ src/main/java/com/batherphilippa/filterapp/utils/FileUtils.java
@@ -1,2 +1,34 @@
-package com.batherphilippa.filterapp.utils;public class FileUtils {
+package com.batherphilippa.filterapp.utils;
+
+import javafx.scene.control.RadioButton;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class FileUtils {
+
+    public static File getFileFromChooser(RadioButton btn) {
+        Stage stage = (Stage) btn.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        return fileChooser.showOpenDialog(stage);
+    }
+
+    public static List<File> getMultipleFilesFromChooser(RadioButton btn) {
+        Stage stage = (Stage) btn.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        return fileChooser.showOpenMultipleDialog(stage);
+    }
+
+    public static File createFile(File file) {
+        File newFile = new File(file.getName());
+        try {
+            newFile.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return newFile;
+    }
 }
