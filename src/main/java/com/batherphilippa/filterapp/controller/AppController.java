e0667aea08aaef97006fb24fdf57c902f29f437a
pabather <p.a.bather1@gmail.com>
2023-11-29 23:19:25 +0100

    anadé funcionalidad del FileChooser y para cargar el ImageController

--- src/main/java/com/batherphilippa/filterapp/controller/AppController.java
+++ src/main/java/com/batherphilippa/filterapp/controller/AppController.java
@@ -1,25 +1,30 @@
 package com.batherphilippa.filterapp.controller;
 
+import com.batherphilippa.filterapp.utils.FileUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import static com.batherphilippa.filterapp.constants.Constants.PATH;
 
 public class AppController implements Initializable {
 
     @FXML
-    private RadioButton rbtnOneFile;
+    private RadioButton radBtnOneFile;
 
     @FXML
     private ToggleGroup fileSelection;
 
     @FXML
-    private RadioButton rbtnMultipleFiles;
+    private RadioButton radBtnMultipleFiles;
 
     @FXML
     private TabPane tpFilterTabManager;
@@ -30,15 +35,38 @@ public class AppController implements Initializable {
     }
 
     @FXML
-    void selectFile(ActionEvent event) {
+    public void handleFileSelection(ActionEvent event) {
 
-        if (rbtnOneFile.isSelected()) {
-            System.out.println("One file is to be chosen.");
+        if (radBtnOneFile.isSelected()) {
+            File file = FileUtils.getFileFromChooser(radBtnOneFile);
+            File newFile = FileUtils.createFile(file);
+            launchImageController(newFile);
         } else {
             System.out.println("Multiples are to be chosen.");
+            List<File> files = FileUtils.getMultipleFilesFromChooser(radBtnMultipleFiles);
+            for (File file:
+                 files) {
+                File newFile = FileUtils.createFile(file);
+                launchImageController(newFile);
+            }
         }
-
     }
 
+    private FXMLLoader launchImageController(File file) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(PATH + "image_progress_pane.fxml"));
+        loader.setController(new ImageController(file));
+        openImageTab(loader, file);
+        return loader;
+    }
+
+    private void openImageTab(FXMLLoader loader, File file) {
+        String tabName = file.getName();
+
+        try {
+            tpFilterTabManager.getTabs().add(new Tab(tabName, loader.load()));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 
 }
