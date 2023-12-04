49003ac778af21c2acc6801bcb5d12dcfb6385c1
pabather <p.a.bather1@gmail.com>
2023-12-04 21:19:45 +0100

    actualicé para obtener los filtros elegidos

--- src/main/java/com/batherphilippa/filterapp/controller/AppController.java
+++ src/main/java/com/batherphilippa/filterapp/controller/AppController.java
@@ -1,6 +1,8 @@
 package com.batherphilippa.filterapp.controller;
 
 import com.batherphilippa.filterapp.utils.FileUtils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,12 +12,16 @@ import javafx.scene.control.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import static com.batherphilippa.filterapp.constants.Constants.PATH;
 
 public class AppController implements Initializable {
+
+    @FXML
+    private Button applyFilters;
 
     @FXML
     private RadioButton radBtnOneFile;
@@ -29,34 +35,50 @@ public class AppController implements Initializable {
     @FXML
     private TabPane tpFilterTabManager;
 
+    @FXML
+    private ListView<String> lvFilterSelection;
+
+    private ObservableList<String> filterOptions = FXCollections.observableArrayList("Escala de Grises",
+            "Inversión de Color", "Aumento de Brillo", "Difuminado de la Imagen");
+
+    private List<File> files = new ArrayList<>();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("App controller initialised...");
+        lvFilterSelection.setItems(filterOptions);
+        lvFilterSelection.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
 
     @FXML
-    public void handleFileSelection(ActionEvent event) {
+    public void handleFileSelection(ActionEvent event) throws IOException {
         if (radBtnOneFile.isSelected()) {
             // un archivo elegido
             File file = FileUtils.getFileFromChooser(radBtnOneFile);
             if (file != null) {
-                launchImageController(file);
+                files.add(file);
             }
         } else {
             // multiples archivos elegidos
-            List<File> files = FileUtils.getMultipleFilesFromChooser(radBtnMultipleFiles);
-            if (!files.isEmpty()) {
-                for (File file:
-                     files) {
-                    launchImageController(file);
-                }
+            files = FileUtils.getMultipleFilesFromChooser(radBtnMultipleFiles);
+        }
+    }
+
+    @FXML
+    private void applyFilters() {
+        List<String> selectedItems = lvFilterSelection.getSelectionModel().getSelectedItems();
+
+        if (files != null) {
+            for (File file :
+                    files) {
+                launchImageController(file, selectedItems);
             }
         }
     }
 
-    private void launchImageController(File file) {
+    private void launchImageController(File file, List<String> selectedFilters) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(PATH + "image_progress_pane.fxml"));
-        loader.setController(new ImageController(file));
+        loader.setController(new ImageController(file, selectedFilters));
         openImageTab(loader, file);
     }
 
@@ -69,5 +91,4 @@ public class AppController implements Initializable {
             ioe.printStackTrace();
         }
     }
-
 }
