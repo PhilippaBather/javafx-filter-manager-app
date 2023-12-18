400156c8312964e1c77141f2ac724bde6e44155a
pabather <p.a.bather1@gmail.com>
2023-12-18 12:13:00 +0100

    updated constants

--- src/main/java/com/batherphilippa/filterapp/constants/Constants.java
+++ src/main/java/com/batherphilippa/filterapp/constants/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 
     public static final String FXML_FILE_PATH = "/com/batherphilippa/filterapp/";
 
-    public static final String IMAGE_FILE_NAME_SUFFIX_TEMP = "_temp";
-    public static final String IMAGE_FILE_PATH = "C:/Users/bathe/OneDrive/Pictures/FilteredImages/";
+    public static final String IMAGE_FILE_NAME_SUFFIX_TEMP = "_filtered";
+    public static final String IMAGE_FILE_PATH = System.getProperty("user.home").concat("/Pictures/FilteredImages/");
     public static final String IMAGE_FILE_TYPE_PNG = ".png";
     public static final String LOG_FILE_NAME = "log";
     public static final String LOG_FILE_TYPE_TXT = ".txt";
