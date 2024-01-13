a23036e199c6ec8628d19089a9c5bd4bfb8b5cc5
pabather <p.a.bather1@gmail.com>
2024-01-13 20:24:08 +0100

    added splash screen controller

--- src/main/java/com/batherphilippa/filterapp/controller/SplashScreenController.java
+++ src/main/java/com/batherphilippa/filterapp/controller/SplashScreenController.java
@@ -1,2 +1,56 @@
-package com.batherphilippa.filterapp.controller;public class SplashScreenController {
+package com.batherphilippa.filterapp.controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static com.batherphilippa.filterapp.constants.FileConstants.FXML_FILE_PATH;
+
+public class SplashScreenController implements Initializable {
+
+    @FXML
+    private AnchorPane paneSplashScreen;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        splashScreenHandler();
+    }
+
+    public void splashScreenHandler() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+            loadAppScreen();
+        }).start();
+    }
+
+    private void loadAppScreen(){
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    FXMLLoader loader = new FXMLLoader(this.getClass().getResource(FXML_FILE_PATH + "main.fxml"));
+                    loader.setController(new AppController());
+                    Scene scene = new Scene(loader.load());
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.show();
+                    paneSplashScreen.getScene().getWindow().hide();
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
+            }
+        });
+    }
 }
